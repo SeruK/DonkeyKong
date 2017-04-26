2d031a3738080e44c6ae55ba3269b18463fba652
SeruK <strandsebastian2000@gmail.com>
2017-04-26 20:57:21 +0200

    * Moved extensions

--- Assets/SATools/Scripts/Core/Extensions.cs
+++ Assets/SATools/Scripts/Core/Extensions.cs
@@ -4,9 +4,9 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 
-public static class ArrayExtensions
+public static class Extensions
 {
-	#region Methods
+	#region Array
 	public static T[] Fill<T>(this T[] array, T value)
 	{
 		for(int i = 0; i < array.Length; ++i)
@@ -37,5 +37,114 @@ public static class ArrayExtensions
 
 		return array;
 	}
-	#endregion // Methods
+	#endregion // Array
+
+	#region Dictionary
+	public static Value FindOrDefault<Key, Value>(this Dictionary<Key, Value> dictionary, Key key, Value defaultValue)
+	{
+		Value value;
+		if(dictionary.TryGetValue(key, out value))
+		{
+			return value;
+		}
+		else
+		{
+			return defaultValue;
+		}
+	}
+
+	public static Value FindOrCall<Key, Value>(this Dictionary<Key, Value> dictionary, Key key, System.Func<Value> defaultValue)
+	{
+		Value value;
+		if(dictionary.TryGetValue(key, out value))
+		{
+			return value;
+		}
+		else
+		{
+			return defaultValue();
+		}
+	}
+
+	public static Value FindOrAddDefault<Key, Value>(this Dictionary<Key, Value> dictionary, Key key, Value defaultValue)
+	{
+		Value value;
+		if(dictionary.TryGetValue(key, out value))
+		{
+			return value;
+		}
+		else
+		{
+			dictionary.Add(key, defaultValue);
+			return defaultValue;
+		}
+	}
+
+	public static Value FindOrAddNew<Key, Value>(this Dictionary<Key, Value> dictionary, Key key)
+		where Value : new()
+	{
+		Value value;
+		if(dictionary.TryGetValue(key, out value))
+		{
+			return value;
+		}
+		else
+		{
+			Value v = new Value();
+			dictionary.Add(key, v);
+			return v;
+		}
+	}
+
+	public static Value FindOrAddDefault<Key, Value>(this Dictionary<Key, Value> dictionary, Key key, System.Func<Value> defaultValue)
+	{
+		Value value;
+		if(dictionary.TryGetValue(key, out value))
+		{
+			return value;
+		}
+		else
+		{
+			var def = defaultValue();
+			dictionary.Add(key, def);
+			return def;
+		}
+	}
+
+	public static Value? FindOrNullable<Key, Value>(this Dictionary<Key, Value> dictionary, Key key) where Value : struct
+	{
+		Value value;
+		if(dictionary.TryGetValue(key, out value))
+		{
+			return (Value?)value;
+		}
+		else
+		{
+			return (Value?)null;
+		}
+	}
+
+	public static Value FindOrNull<Key, Value>(this Dictionary<Key, Value> dictionary, Key key) where Value : class
+	{
+		Value value;
+		if(dictionary.TryGetValue(key, out value))
+		{
+			return value;
+		}
+		else
+		{
+			return null;
+		}
+	}
+	#endregion // Dictionary
+
+	#region GameObject
+	public static T AddMissingComponent<T>(this GameObject go)
+		where T : Component
+	{
+		var obj = go.GetComponent<T>();
+		if(obj == null) { obj = go.AddComponent<T>(); }
+		return obj;
+	}
+	#endregion // GameObject
 }
