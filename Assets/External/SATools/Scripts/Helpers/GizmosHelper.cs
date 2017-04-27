4dc989b88cb95d483f2391fe15e066b89e173d48
SeruK <strandsebastian2000@gmail.com>
2017-04-27 19:18:59 +0200

    * Drawing platforms

---
+++ Assets/External/SATools/Scripts/Helpers/GizmosHelper.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UE = UnityEngine;
+using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+
+public static class GizmosHelper
+{
+	[Conditional("UNITY_EDITOR")]
+	public static void Scope( Action action )
+	{
+		if(action == null) { return; }
+
+		var oldMatrix = Gizmos.matrix;
+		var color = Gizmos.color;
+
+		action();
+
+		Gizmos.matrix = oldMatrix;
+		Gizmos.color = color;
+	}
+
+	[Conditional("UNITY_EDITOR")]
+	public static void DrawRect(Rect r, float z)
+	{
+		DrawRect(
+			bl: new Vector3(r.x, r.y, z),
+			br: new Vector3(r.x + r.width, r.y, z),
+			tl: new Vector3(r.x, r.y + r.height, z),
+			tr: new Vector3(r.x + r.width, r.y + r.height, z)
+		);
+	}
+
+	[Conditional("UNITY_EDITOR")]
+	public static void DrawRect(Vector3 bl, Vector3 br, Vector3 tl, Vector3 tr)
+	{
+		Gizmos.DrawLine(bl, br);
+		Gizmos.DrawLine(br, tr);
+		Gizmos.DrawLine(tr, tl);
+		Gizmos.DrawLine(tl, bl);
+	}
+}
---
+++ Assets/Scripts/Platform.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UE = UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public sealed class Platform : MonoBehaviour
+{
+	#region Types
+	#region Serialized Types
+#pragma warning disable 0649
+#pragma warning restore 0649
+	#endregion // Serialized Types
+	#endregion // Types
+
+	#region Fields
+	#region Serialized Fields
+#pragma warning disable 0649
+#pragma warning restore 0649
+	#endregion // Serialized Fields
+	#endregion // Fields
+
+	#region Properties
+	#endregion // Properties
+
+	#region Mono
+	#endregion // Mono
+
+	#region Methods
+	protected void OnDrawGizmos()
+	{
+		var collider = GetComponent<BoxCollider2D>();
+		if(collider == null) { return; }
+
+		GizmosHelper.Scope(() =>
+		{
+			Gizmos.matrix = transform.localToWorldMatrix;
+
+			Gizmos.DrawWireCube(collider.offset, collider.size);
+		});
+	}
+	#endregion // Methods
+}
